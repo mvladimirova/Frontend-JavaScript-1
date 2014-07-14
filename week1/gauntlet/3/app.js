8a54148c1412c8ae82bdca11397d58e90a412164
Marianna <marianna.vl.vladimirova@gmail.com>
2014-07-14 23:23:13 +0300

    Adding and removing pictures done

--- week1/gauntlet/3/app.js
+++ week1/gauntlet/3/app.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
+  "use strict";
 
+  var inputField = $("#search-input");
+  var galery = $(".container");
+
+  var loadImage = function(){
+    var url = inputField.val();
+    var img = $("<img>");
+    img.load(function(){
+      galery.append(img);
+    });
+
+    img.attr("src", url);
+    img.on("click", function(){
+      $(this).remove();
+    });
+  };
+  $("#search-button").on("click", loadImage);
 });
