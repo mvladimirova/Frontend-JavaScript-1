87d51496de1046d8d01d6e3872699a19b7c2b5c5
Marianna <marianna.vl.vladimirova@gmail.com>
2014-07-14 23:00:59 +0300

    Adding functionnality for highlight

--- week1/gauntlet/2/app.js
+++ week1/gauntlet/2/app.js
@@ -1,1 +1,20 @@
-console.log("Empty I am.");
+"use strict";
+
+$(document).ready(function(){
+  var paragraphs = [];
+
+  var children = $("#mightyParagraphHolder").children();
+  children.each(function (par){
+    paragraphs.push(children[par]);
+  });
+
+  $("button").on("click", function(){
+    $("p").removeClass(".highlight");
+
+    var current = paragraphs.shift();
+    $(current).addClass(".highlight");
+    paragraphs.push(current);
+  });
+
+});
+
