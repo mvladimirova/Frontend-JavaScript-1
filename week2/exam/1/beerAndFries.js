886e8ef31d8ab35b5b320dceffcd5e93b144ff73
Marianna Vladimirova <marianna.vl.vladimirova@gmail.com>
2014-07-28 15:10:13 +0300

    Edit style chages

--- week2/exam/1/beerAndFries.js
+++ week2/exam/1/beerAndFries.js
@@ -1,41 +1,45 @@
-"use strict"
+"use strict";
+
+var groupBy = function(groupingFunction, arr) {
+  var result = {};
+
+  arr.forEach(function(x) {
+    var key = groupingFunction(x);
+
+    if (result[key]) {
+      result[key].push(x);
+    } else {
+      result[key] = [x];
+    }
+  return result;
+  });
+};
+
+var numSort = function(a,b){
+  return b - a;
+};
+
 
 var beerAndFries = function(items){
-  
+
+  var beerScores = [],
+      friesScores = [],
+      maxScore = 0;
   if(items.length === 0){
     return 0;
   }
 
-  var groupBy = function(groupingFunction, arr) {
-    var result = {};
-
-    arr.forEach(function(x) {
-      var key = groupingFunction(x);
-
-      if (result[key]) {
-        result[key].push(x);
-      } else {
-        result[key] = [x];
-      }
-    });
-
-    return result;
-  };
-
-  var groupedByType = groupBy(function(x){return x.type}, items);
-  var beerScores = [];
-  var friesScores = [];
+  var groupedByType = groupBy(function(x){return x.type;}, items);
   groupedByType.beer.forEach(function(x){
-    beerScores.push(x.score);  
+    beerScores.push(x.score);
   });
   groupedByType.fries.forEach(function(x){
     friesScores.push(x.score);
   });
 
-  var sortedBeerScores = beerScores.sort(function(a,b){return b-a});
-  var sortedFriesScores = friesScores.sort(function(a,b) {return b-a});
-  
-  var maxScore = 0;
+  var sortedBeerScores = beerScores.sort(numSort(a,b));
+  var sortedFriesScores = friesScores.sort(numSort(a,b));
+
   sortedBeerScores.forEach(function(currentBeer, index){
     maxScore += currentBeer * sortedFriesScores[index];
   });
