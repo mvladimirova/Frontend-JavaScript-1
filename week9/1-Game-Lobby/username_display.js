ff0757706099421282e5f47ce6de27483a9d89ad
Marianna <marianna.vl.vladimirova@gmail.com>
2014-08-05 21:13:49 +0300

    Adding changes

--- week9/1-Game-Lobby/username_display.js
+++ week9/1-Game-Lobby/username_display.js
@@ -1,7 +1,13 @@
 define(["backbone", "jquery"], function(Backbone, $){
   var UsernameDisplay = Backbone.View.extend({
+    events: {
+      "click .saveButton" : "save"
+    },
     initialize: function(){
       this.model.on("change:username", this.render, this);
+    },
+    save: function(){
+      this.model.set("username", this.$el.find("input").val());
     },
     render: function() {
       var parseHtml = $("#inputUsernameTemplate").html();
