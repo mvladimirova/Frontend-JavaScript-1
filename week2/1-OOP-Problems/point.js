2f1110204b2251e7cac7be916f4e1b5ee80a551a
Marianna <marianna.vl.vladimirova@gmail.com>
2014-08-16 16:50:53 +0300

    Adding point

---
+++ week2/1-OOP-Problems/point.js
@@ -0,0 +1,35 @@
+"use strict";
+
+function Point(x,y){
+  this.x = x;
+  this.y = y;
+
+  this.xInc = function(){
+    return this.x + 1;
+  };
+
+  this.xDec = function(){
+    return this.x - 1;
+  };
+
+  this.yInc = function(){
+    return this.y + 1;
+  };
+
+  this.yDec = function(){
+    return this.y - 1;
+  };
+}
+
+Point.prototype.equals = function(point){
+  return this.x === point.x && this.y === point.y;
+};
+
+Point.prototype.toString = function(){
+  return ["Point @ ", this.x ,", ", this.y].join("");
+};
+
+var p = new Point(1,2);
+var p1 = new Point(2,3);
+console.log(p.equals(p1));
+console.log(p.toString());
